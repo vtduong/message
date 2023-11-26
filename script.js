1e9c78e8c470e1855aada75cfd5c6cc5d2b917e4
vtduong <vanduong1610@gmail.com>
2023-11-25 22:17:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ yesBtn.addEventListener("click", () => {
   question.innerHTML = "Yay, see you on the 18th!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+  yesBtn.sytle.visibility = "hidden";
 });
 
 noBtn.addEventListener("mouseover", () => {
@@ -20,4 +21,5 @@ noBtn.addEventListener("mouseover", () => {
 
   noBtn.style.left = randomX + "px";
   noBtn.style.top = randomY + "px";
+  noBtn.sytle.visibility = "hidden";
 });
